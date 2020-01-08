3843b7754b0c7485482e8ed4f569245f7c157eb3
Robert Shields <robert.shieldsiv@gmail.com>
2020-01-08 03:04:37 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+
+  attr_reader :name , :species
+  
+  @@all = []
+  
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+  
+  def species
+    @species = "human"
+  end
+  
+  def say_species
+    return "I am a human."
+  end
+  
+  def self.all
+  @@all
+  end
+  
+  def.self.count
+  end
+
+  
 end
